d20915bc65f39cc2f0c14e94f18a3abf71fcbd97
Priscilla H <priscillamhuynh@gmail.com>
2019-10-21 09:24:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
-def position_taken?(board,index)
-  if (board[index]) == " " || board[index] == ""|| board[index]== nil
-    taken = false
-  else
-    taken = true
-end# code your #position_taken? method here!
+# def position_taken?(board,index)
+#   if (board[index]) == " " || board[index] == ""|| board[index]== nil
+#     taken = false
+#   else
+#     taken = true
+# end# code your #position_taken? method here!
 
   def position_taken?(board,index)
     if (board[index]) == " " || board[index] == ""|| board[index]== nil
